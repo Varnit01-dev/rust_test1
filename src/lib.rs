23cd65be3d932d033aa95bee3aedd5c443501e25
Varnit Sharma <varnitsharma2015@gmail.com>
2025-07-29 18:40:41 +0530

    feat: Added Hello world and cargo.toml

---
+++ src/lib.rs
@@ -0,0 +1,37 @@
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    entrypoint,
+    entrypoint::ProgramResult,
+    msg,
+    program_error::ProgramError,
+    pubkey::Pubkey,
+};
+
+
+entrypoint!(process_instruction);
+
+
+pub fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    _instruction_data: &[u8],
+) -> ProgramResult {
+    msg!("Hello World Rust program entrypoint");
+
+   
+    let accounts_iter = &mut accounts.iter();
+
+    
+    let account = next_account_info(accounts_iter)?;
+
+   
+    if account.owner != program_id {
+        msg!("Greeted account does not have the correct program id");
+        return Err(ProgramError::IncorrectProgramId);
+    }
+
+    msg!("Hello {}!", account.key);
+    msg!("Program successfully executed!");
+
+    Ok(())
+}
